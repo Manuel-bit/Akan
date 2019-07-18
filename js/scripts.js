b0bcf1c16c1b43cdc60fe72228726dcef89c4f66
manuel.bit <emmanuel_njuguna@outlook.com>
2019-07-18 16:17:00 +0300

    add variables for form input

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+var= century, year, month, day, gender;
+var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'saturday'];
+var maleName = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+var femaleName = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+var date=[];
+function getData(){
+  century=document.getElementById('cc').value;
+  date.push(century);
+  year= document.getElementById('yy').value;
+  date.push(year);
+  moth= document.getElementById('mm').value;
+  date.push(month);
+  day= document.getElementById('dd').value;
+  date.push(day);
+  gender= document.getElementById('gender').value;
+  checkData();
+}
+function checkData(){
+  if(date[2]<= 0 && date[2]> 12){
+    alert("invalid month");
+    return;
+  }
+  if(date[3]<= o && date[3]> 31){
+    alert("invalid day");
+    return;
+  }
+  final();
+}
+function calculateName(){
+   return (((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day) % 7;
+}
+function final(){
+  var result= parseInt(calculateName());
+  if
+}
